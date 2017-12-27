riscv-rust/riscv-rust-toolchain

commit 00002c75c357748977dd973e7b7e5bfefaa96ebf

Add "all" and "clean" targets

Makefile
@@ -2,6 +2,12 @@ root_dir           := $(CURDIR)
 build_dir          := $(root_dir)/build
 sysroot_dir        := $(root_dir)/toolchain
 
+
+all: toolchain
+
+clean:
+	git clean -qdfx
+
 toolchain: llvm openocd binutils rust xargo
 	@echo All Tools Installed
 

commit a1e9217884233fbca10c0feaf7481aa87e04f94e

Sanitize the "clean" target
`git clean -qfdx` is useful, but also unexpected for a "clean" target.  Replace
 the command with a more sane `$(RM) -r build toolchain`.

Fixes: 00002c75c357748977dd973e7b7e5bfefaa96ebf

Signed-Off-By: Dennis Schridde <devurandom@gmx.net>

Makefile
@@ -10,7 +10,7 @@ ccache             := $(shell which ccache)
 all: toolchain
 
 clean:
-	git clean -qdfx
+	$(RM) -r build toolchain
 
 toolchain: llvm openocd binutils rust xargo
 	@echo All Tools Installed